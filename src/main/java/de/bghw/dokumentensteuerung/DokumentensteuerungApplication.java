d1619c41eae61c0b63221908fd658eb41475ae3d
xf0e <droidlove@ya.ru>
2020-11-08 21:21:22 +0100

    init

---
+++ src/main/java/de/bghw/dokumentensteuerung/DokumentensteuerungApplication.java
@@ -0,0 +1,13 @@
+package de.bghw.dokumentensteuerung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokumentensteuerungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokumentensteuerungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bghw/dokumentensteuerung/DokumentensteuerungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bghw.dokumentensteuerung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokumentensteuerungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
